035a0e8a66d94b2f46f96a0cce892a3cc44bc5e9
Anders Ingemann <aim@orbit.online>
2024-01-30 15:59:40 +0100

    upkg: Remove usage of `upkg root`

--- git-release.sh
+++ git-release.sh
@@ -2,7 +2,7 @@
 
 main() {
   set -eo pipefail; shopt -s inherit_errexit
-  local pkgroot; pkgroot=$(upkg root "${BASH_SOURCE[0]}")
+  local pkgroot; pkgroot=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
   source "$pkgroot/.upkg/orbit-online/records.sh/records.sh"
   DOC="git-release - Tag REF by either bumping the last version or specifying one
 Usage:
